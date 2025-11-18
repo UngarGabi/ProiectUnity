e9d87be162dfa5401f5ca69f2b0e0208ec4edc99
UngarGabi <ungar.gabi24@gmail.com>
2025-11-19 00:45:16 +0200

    map changes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject treePrefab;
+    private float spawnZoneX;
+    private float spawnZoneY;
+    public float prefabCount;
+    public float YRangeTrees = 400;
+    public float xRangeTrees = 100;
+    void Start()
+    {
+        SpawnTrees();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SpawnTrees()
+    {
+        for (int i = 0; i < prefabCount; i++) {
+            spawnZoneX = Random.Range(xRangeTrees, YRangeTrees);
+            spawnZoneY = Random.Range(xRangeTrees, YRangeTrees);
+
+            Vector3 treePosition = new Vector3(spawnZoneX, 0, spawnZoneY);
+            Instantiate(treePrefab, treePosition, treePrefab.GetComponent<Transform>().rotation);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,22 @@
+Death by NightTime.
 
-Scriptul controleaza miscarea si rotatia unui personaj third-person.
+-Un joc inspirat din Dead by Daylight.
+
+
+
+Pana acum:
+
+Un script controleaza miscarea si rotatia unui personaj third-person.
 Miscarea este relativa la directia camerei, iar modelul vizual se roteste spre directia de mers.
 Deplasarea este realizata prin Rigidbody, nu prin transform direct.
 
+S-a creat harta si s-a format terenul folosind Terrain.
+
+S-au adaugat asset-uri utile si s-au facut prefab-uri.
+
+S-a creat un script pentru generarea copacilor la inceputul jocului.
+
+
+
+Dorim ca review-ul sa fie trimis pe Teams, la Ungar Gabriel.
 
